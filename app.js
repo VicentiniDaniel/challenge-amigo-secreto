203b620e05790a94d02470610180a74e8c7dfcc7
Daniel Vicentini <ddvicentin@gmail.com>
2025-01-23 21:57:47 +0100

    adicionando amigos

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 let nomes = [];
+
+function adicionarAmigo() {
+    const input = document.getElementById('amigo');
+    const nome = input.value.trim();
+
+    if (nome !== "") {
+        nomes.push(nome);
+        input.value = "";
+    } 
