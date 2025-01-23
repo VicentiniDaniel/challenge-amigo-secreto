bfa8a4847f0ffdca4ff04f2af7443747c101826e
Daniel Vicentini <ddvicentin@gmail.com>
2025-01-23 22:16:43 +0100

    nomes sorteados nao se repetem

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ function adicionarAmigo() {
         nomes.push(nome);
         document.getElementById('listaAmigos').innerHTML += `<li>${nome}</li>`;
         input.value = "";
-    } 
-    else {
+    } else {
         alert("Por favor, insira um nome válido.");
     }
 }
@@ -17,9 +16,9 @@ function adicionarAmigo() {
 function sortearAmigo() {
     if (nomes.length > 0) {
         const indiceAleatorio = Math.floor(Math.random() * nomes.length);
-        const amigoSecreto = nomes[indiceAleatorio];
+        const amigoSecreto = nomes.splice(indiceAleatorio, 1)[0];
         document.getElementById('resultado').innerText = `O amigo secreto é: ${amigoSecreto}`;
     } else {
-        alert("A lista de nomes está vazia.");
+        alert("A lista de nomes está vazia ou todos os nomes já foram sorteados.");
     }
 }
